0286699c6a1d1fbe0d05b99f5e5a7c39c57b18d8
elvis <elvis@lnmpy.com>
2016-09-21 13:41:48 +0800

    remove for build

--- src/module.js
+++
@@ -1,5 +0,0 @@
-require('../bower_components/angular-schema-form/dist/schema-form.js');
-require('./angular-schema-form-material-templates.js');
-require('./type-parser.js');
-require('./material-decorator.js');
-require('./material-class.js');
